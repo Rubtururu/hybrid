3d7f0add792f2ad297dadf88b1d6ad43cb12dbd9
Rubtururu <59855904+Rubtururu@users.noreply.github.com>
2025-12-31 11:05:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const CONTRACT_ADDRESS = "0x2A9C22f5b3Ccf204D1d7d11305a8F1D2FF5AbaE2";
 const ABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"boost","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"duration","type":"uint256"}],"name":"Boost","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Claim","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Loan","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"RaffleEnter","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"winner","type":"address"},{"indexed":false,"internalType":"uint256","name":"prize","type":"uint256"}],"name":"RaffleWin","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Withdraw","type":"event"},{"inputs":[],"name":"accDividendPerShare","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"boostBP","type":"uint256"},{"internalType":"uint256","name":"duration","type":"uint256"}],"name":"activateBoost","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"claim","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"deposit","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"dividendPool","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"drawRaffleWinner","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"effectiveShares","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"enterRaffle","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"lastDividendTime","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"lastVolumeReset","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"pendingDividends","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"rafflePlayers","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"rafflePot","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"uint256","name":"duration","type":"uint256"}],"name":"takeLoan","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"totalDeposited","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalDividendsPaid","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalShares","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalUsers","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalWithdrawn","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"treasuryPool","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"users","outputs":[{"internalType":"uint256","name":"deposited","type":"uint256"},{"internalType":"uint256","name":"withdrawn","type":"uint256"},{"internalType":"uint256","name":"shares","type":"uint256"},{"internalType":"uint256","name":"rewardDebt","type":"uint256"},{"internalType":"uint256","name":"dividendsClaimed","type":"uint256"},{"internalType":"uint256","name":"lastAction","type":"uint256"},{"internalType":"uint256","name":"boostBP","type":"uint256"},{"internalType":"uint256","name":"boostEnd","type":"uint256"},{"internalType":"uint256","name":"loanAmount","type":"uint256"},{"internalType":"uint256","name":"loanEnd","type":"uint256"},{"internalType":"bool","name":"exists","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"volume24h","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive"}];
+
 let web3, contract, account, chart;
 
 document.getElementById("connectBtn").onclick = connect;
@@ -14,14 +15,19 @@ async function connect(){
   if(window.ethereum){
     web3 = new Web3(window.ethereum);
     try{
-      const accounts = await ethereum.request({ method:'eth_requestAccounts' });
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
       account = accounts[0];
-      const chainId = await ethereum.request({ method:'eth_chainId' });
+
+      const chainId = await window.ethereum.request({ method: 'eth_chainId' });
       if(chainId !== '0x61'){ alert("Cambia MetaMask a BSC Testnet"); return; }
+
       contract = new web3.eth.Contract(ABI, CONTRACT_ADDRESS);
+      alert("Wallet conectada: " + account);
+
       loadStats();
       initChart();
-    }catch(err){ alert(err.message); }
+
+    } catch(err){ alert("Error MetaMask: " + err.message); }
   } else { alert("Instala MetaMask"); }
 }
 
@@ -60,29 +66,16 @@ async function loadStats(){
     document.getElementById("loan").innerText = u.loanAmount>0?web3.utils.fromWei(u.loanAmount)+" BNB":'-';
 
     updateChart(tvl, divPool);
-  }catch(err){ console.error(err); }
+  } catch(err){ console.error(err); }
 }
 
-async function deposit(){
-  const amount = document.getElementById("amount").value;
-  if(!amount) return alert("Ingresa cantidad");
-  await contract.methods.deposit().send({ from: account, value: web3.utils.toWei(amount) });
-  loadStats();
-}
-
-async function withdraw(){
-  const amount = document.getElementById("amount").value;
-  if(!amount) return alert("Ingresa cantidad");
-  await contract.methods.withdraw(web3.utils.toWei(amount)).send({ from: account });
-  loadStats();
-}
-
+async function deposit(){ const amount = document.getElementById("amount").value; if(!amount) return alert("Ingresa cantidad"); await contract.methods.deposit().send({ from: account, value: web3.utils.toWei(amount) }); loadStats(); }
+async function withdraw(){ const amount = document.getElementById("amount").value; if(!amount) return alert("Ingresa cantidad"); await contract.methods.withdraw(web3.utils.toWei(amount)).send({ from: account }); loadStats(); }
 async function claim(){ await contract.methods.claim().send({from:account}); loadStats(); }
 async function activateBoost(){ const amt = prompt("BNB para boost"); if(amt) await contract.methods.activateBoost(web3.utils.toWei(amt),3600*24).send({from:account}); loadStats(); }
 async function takeLoan(){ const amt = prompt("BNB préstamo"); if(amt) await contract.methods.takeLoan(web3.utils.toWei(amt),3600*24).send({from:account}); loadStats(); }
 async function enterRaffle(){ await contract.methods.enterRaffle().send({from:account,value:web3.utils.toWei('0.01')}); loadStats(); }
 
-// Chart
 function initChart(){
   const ctx = document.getElementById('chart').getContext('2d');
   chart = new Chart(ctx,{ type:'line', data:{ labels:[], datasets:[
@@ -97,10 +90,7 @@ function updateChart(tvl, div){
   chart.data.labels.push(time);
   chart.data.datasets[0].data.push(web3.utils.fromWei(tvl));
   chart.data.datasets[1].data.push(web3.utils.fromWei(div));
-  if(chart.data.labels.length>30){
-    chart.data.labels.shift();
-    chart.data.datasets.forEach(d=>d.data.shift());
-  }
+  if(chart.data.labels.length>30){ chart.data.labels.shift(); chart.data.datasets.forEach(d=>d.data.shift()); }
   chart.update();
 }
 
